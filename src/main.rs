45d3c80ae304012c2f2405304efba1f48971e97a
Brian Beattie <beattie@beattie-home.net>
2025-04-04 18:15:52 -0700

    Add user button interrupt

--- src/main.rs
+++ src/main.rs
@@ -8,20 +8,56 @@
 // use panic_itm as _; // logs messages over ITM; requires ITM support
 use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
 
-use cortex_m_rt::entry;
+use core::cell::RefCell;
 
-use stm32f3xx_hal::{pac::Peripherals, delay::Delay, prelude::*};
+use cortex_m_rt::entry;
+use cortex_m_semihosting::hprintln;
+use critical_section::Mutex;
+ 
+use stm32f3xx_hal::{
+    interrupt,
+    gpio::{self, Edge, Input},
+    pac::{Peripherals, NVIC},
+    delay::Delay,
+    prelude::*
+};
+use stm32f3xx_hal::pac::Interrupt;
+
+type ButtonPin = gpio::PA0<Input>;
+
+static mut  RUN:bool = true;
+
+static BUTTON: Mutex<RefCell<Option<ButtonPin>>> = Mutex::new(RefCell::new(None));
 
 #[entry]
 fn main() -> ! {
+    hprintln!("Hello from stm32f3discovery quickstart");
     let dp = Peripherals::take().unwrap();
     let cp = cortex_m::peripheral::Peripherals::take().unwrap();
 
     let mut rcc = dp.RCC.constrain();
     let mut flash = dp.FLASH.constrain();
     let mut delay = Delay::new(cp.SYST, rcc.cfgr.freeze(&mut flash.acr));
+    let mut syscfg = dp.SYSCFG.constrain(&mut rcc.apb2);
 
+    let mut exti = dp.EXTI;
     let mut gpioe = dp.GPIOE.split(&mut rcc.ahb);
+    let mut gpioa = dp.GPIOA.split(&mut rcc.ahb);
+
+    // Configuring the user button to trigger an interrupt when the button is pressed.
+    let mut user_button = gpioa.pa0.into_pull_down_input(&mut gpioa.moder, &mut gpioa.pupdr);
+    syscfg.select_exti_interrupt_source(&user_button);
+    user_button.trigger_on_edge(&mut exti, Edge::Rising);
+    user_button.enable_interrupt(&mut exti);
+    let interrupt_num = user_button.interrupt(); // hal::pac::Interrupt::EXTI0
+
+    // Moving ownership to the global BUTTON so we can clear the interrupt pending bit.
+    critical_section::with(|cs| *BUTTON.borrow(cs).borrow_mut() = Some(user_button));
+
+
+    unsafe {
+        NVIC::unmask(Interrupt::EXTI0);
+    }
 
 
     let mut leds = [
@@ -47,16 +83,30 @@ fn main() -> ! {
         led.set_low().unwrap();
         delay.delay_ms(50u32);
     }
+
     loop {
         for led in leds.iter_mut() {
-            led.set_high().unwrap();
-            delay.delay_ms(450u16);
-            led.set_low().unwrap();
-            delay.delay_ms(50u16);
+            if unsafe {RUN == true} {
+                led.set_high().unwrap();
+                delay.delay_ms(450u16);
+                led.set_low().unwrap();
+                delay.delay_ms(50u16);
+            }
         }
-        /*
-        leds[0].toggle().unwrap();
-        delay.delay_ms(1000u32);
-        */
     }
+}
+
+#[interrupt]
+fn EXTI0() {
+    hprintln!("User Button");
+    critical_section::with(|cs| {
+        // Clear the interrupt pending bit so we don't infinitely call this routine
+        BUTTON
+            .borrow(cs)
+            .borrow_mut()
+            .as_mut()
+            .unwrap()
+            .clear_interrupt();
+        unsafe {if RUN { RUN = false } else { RUN = true} };
+    })
 }
