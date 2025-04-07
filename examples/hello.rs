0320fdefa8901b24e7a61a5c5ed9f625c294bb05
Brian Beattie <beattie@beattie-home.net>
2025-04-07 14:16:24 -0700

    Modify and test hello semihosting example

--- examples/hello.rs
+++ examples/hello.rs
@@ -2,19 +2,17 @@
 
 #![no_main]
 #![no_std]
+#![allow(unused_imports)]
 
-use panic_halt as _;
+use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
 
 use cortex_m_rt::entry;
-use cortex_m_semihosting::{debug, hprintln};
+use cortex_m_semihosting::hprintln;
+use stm32f3xx_hal::{self, interrupt};
 
 #[entry]
 fn main() -> ! {
     hprintln!("Hello, world!");
-
-    // exit QEMU
-    // NOTE do not run this on hardware; it can corrupt OpenOCD state
-    debug::exit(debug::EXIT_SUCCESS);
 
     loop {}
 }
