1897522e0d4c9fd2ec9d91b3b77e230121453405
Brian Beattie <beattie@beattie-home.net>
2025-04-07 14:16:51 -0700

    New example for serial read write
    Create a new example for serial port on the st-link vcom. One use
    for this is to debug when openocd is not running.

---
+++ examples/serial.rs
@@ -0,0 +1,40 @@
+#![no_std]
+#![no_main]
+use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
+
+use cortex_m_rt::entry;
+// use panic_halt as _;
+use stm32f3xx_hal::{pac, prelude::*, serial};
+
+#[entry]
+fn main() -> ! {
+    let dp = pac::Peripherals::take().unwrap();
+    let mut rcc = dp.RCC.constrain();
+    let mut flash = dp.FLASH.constrain();
+    let clocks = rcc.cfgr.sysclk(48.MHz()).freeze(&mut flash.acr);
+
+    // Configure GPIO pins PC4 and PC5 for UART alternate function
+    let mut gpioc = dp.GPIOC.split(&mut rcc.ahb);
+    let tx = gpioc.pc4.into_af_push_pull::<7>(&mut gpioc.moder, &mut gpioc.otyper, &mut gpioc.afrl);
+    let rx = gpioc.pc5.into_af_push_pull::<7>(&mut gpioc.moder, &mut gpioc.otyper, &mut gpioc.afrl);
+
+    // Configure UART1 (or your desired UART)
+    let mut serial = serial::Serial::new(
+        dp.USART1,
+        (tx, rx),
+        115200.Bd(), // Set your desired baud rate
+        clocks,
+        &mut rcc.apb2,
+    );
+
+    loop {
+        match serial.read() {
+            Ok(byte) => {
+                serial.write(byte).unwrap();
+            }
+            Err(_error) => {
+                continue;
+            }
+        };
+    }
+}
